b1e8d889c2d83189c81ba998fc50fc42f01e570b
Thomas Schäfer <t.schaefer@headtrip.eu>
2019-07-25 17:02:27 +0200

    Add links to douyu streamer pages

--- app/src/Components/Table/PlayerName.tsx
+++ app/src/Components/Table/PlayerName.tsx
@@ -4,14 +4,29 @@ interface Props {
   playerName: string
 }
 
-const twitchFlag = 'tv/'
+const TWITCH = 'tv/'
+const DOYU = '斗鱼'
+// const HUYA = '虎牙'
 
-const PlayerName = ({ playerName }: Props) => {
-  if (!playerName.includes(twitchFlag)) {
-    return <span>{playerName}</span>
+const DOUYO_REGEXP = /(\d[\d]+)/g
+
+type StreamTypes = 'twitch' | 'douyu' | 'huya' | null
+
+const getSteamtype = (playerName: string): StreamTypes => {
+  if (playerName.includes(TWITCH)) {
+    return 'twitch'
   }
+  if (playerName.includes(DOYU) && playerName.match(DOUYO_REGEXP)) {
+    return 'douyu'
+  }
+  // if (playerName.includes(HUYA)) {
+  //   return 'huya'
+  // }
+  return null
+}
 
-  const startIndex = playerName.indexOf(twitchFlag) + 3
+const TwitchName = ({ playerName }: Props) => {
+  const startIndex = playerName.indexOf(TWITCH) + 3
   const channel = playerName.substr(startIndex)
   const link = `https://twitch.tv/${channel}`
   return (
@@ -19,11 +34,53 @@ const PlayerName = ({ playerName }: Props) => {
       href={link}
       target='_blank'
       rel='noopener noreferrer'
-      style={{color: 'white'}}
+      style={{ color: 'white' }}
     >
       {playerName}
+      <span style={{ marginLeft: 10 }}>
+        <i className='fab fa-twitch' />
+      </span>
     </a>
   )
+}
+
+const DouyuName = ({ playerName }: Props) => {
+  const matches = playerName.match(DOUYO_REGEXP)
+  if (!matches || matches.length === 0) {
+    return <BasicName playerName={playerName} />
+  }
+  let channelID = matches.pop()
+  const link = `https://www.douyu.com/${channelID}`
+  return (
+    <a
+      href={link}
+      target='_blank'
+      rel='noopener noreferrer'
+      style={{ color: 'white' }}
+    >
+      {playerName}
+      <span style={{ marginLeft: 10 }}>
+        <i className='fas fa-video' />
+      </span>
+    </a>
+  )
+}
+
+const PlayerName = ({ playerName }: Props) => {
+  const streamType = getSteamtype(playerName)
+  switch (streamType) {
+    case 'douyu':
+      return <DouyuName playerName={playerName} />
+    case 'twitch':
+      return <TwitchName playerName={playerName} />
+    case null:
+    default:
+      return <BasicName playerName={playerName} />
+  }
+}
+
+const BasicName = ({ playerName }: Props) => {
+  return <span>{playerName}</span>
 }
 
 export default PlayerName
