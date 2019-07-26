868c4e4f58dce3fadefbe0ecd3cc6fe6137f0eb1
Thomas Schäfer <t.schaefer@headtrip.eu>
2019-07-26 15:07:32 +0200

    add more twitch streamers

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -4,6 +4,7 @@ interface KnownStreamers {
 
 export const KNOWN_TWITCH_STREAMERS: KnownStreamers = {
   'twobiers': 'https://www.twitch.tv/twobiers/',
+  'twobiers#4033': 'https://www.twitch.tv/twobiers/',
   'cris': 'https://www.twitch.tv/crisheroes',
   'szychu': 'https://www.twitch.tv/szychu',
   'p4wnyhof': 'https://www.twitch.tv/P4wnyhof',
@@ -14,4 +15,9 @@ export const KNOWN_TWITCH_STREAMERS: KnownStreamers = {
   '.tv/quasar273 #2': 'https://www.twitch.tv/quasar273',
   '.tv/quasar273 #3': 'https://www.twitch.tv/quasar273',
   '/quasar273': 'https://www.twitch.tv/quasar273',
+  'hoej': 'https://www.twitch.tv/hoej_hs',
+  'shtan': 'https://www.twitch.tv/shtan_udachi',
+  'bebe872': 'https://www.twitch.tv/bebe872',
+  'kondo, marie': 'https://www.twitch.tv/strifecro',
+  'gaara': 'https://www.twitch.tv/gaara',
 }
