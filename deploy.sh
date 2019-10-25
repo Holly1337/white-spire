6814a1557598fcf3bf53ef910031110673eb8b90
Thomas Schäfer <t.schaefer@headtrip.eu>
2019-10-25 21:06:46 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+echo 'pull and checkout repo'
+git pull origin master
+git checkout master
+echo 'build reactjs'
+(
+  cd app || exit
+  npm run build
+  rm -rf build/api
+  rm -rf build/example_api
+)
+echo 'stop express server'
+(
+  cd express-server || exit
+  npm run stop
+)
+echo 'copy build'
+rm -rf express-server/build
+cp -R app/build express-server/build
+echo 'start express server'
+cd express-server || exit
+npm run start
