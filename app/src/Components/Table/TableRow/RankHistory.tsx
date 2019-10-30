a2e49c910125361c235a271edc53caf1b0e15685
Thomas Schäfer <t.schaefer@headtrip.eu>
2019-10-30 15:36:09 +0100

    Add domains to rank chart <YAxis /> and add <Legend />

--- app/src/Components/Table/TableRow/RankHistory.tsx
+++ app/src/Components/Table/TableRow/RankHistory.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {
-  LineChart, Line, XAxis, YAxis, ResponsiveContainer, CartesianGrid, Label
+  LineChart, Line, XAxis, YAxis, ResponsiveContainer, CartesianGrid, Label, AxisDomain, Legend
 } from 'recharts'
+import { getRankDomain, getScoreDomain } from '../../../lib/chartDomains'
 
 interface Props {
   data: RankData[]
@@ -10,6 +11,12 @@ interface Props {
 const RankHistory: React.FC<Props> = ({ data }) => {
   // limit to last 48 entries
   data = [...data].reverse().slice(0, 48)
+
+  const rankDomain = getRankDomain(data)
+  const scoreDomain = getScoreDomain(data)
+  console.log(rankDomain)
+  console.log(scoreDomain)
+
   return (
     <ResponsiveContainer width={'100%'} aspect={2 / 1}>
       <LineChart
@@ -25,8 +32,9 @@ const RankHistory: React.FC<Props> = ({ data }) => {
           }}
         />
         <CartesianGrid stroke='#666666' vertical={false} />
-        <YAxis yAxisId={'left'} stroke='white' reversed={true} />
-        <YAxis yAxisId={'right'} orientation='right' stroke='#ffff65' reversed={false} />
+        <YAxis yAxisId={'left'} orientation='left' stroke='white' reversed={true} domain={rankDomain} />
+        <YAxis yAxisId={'right'} orientation='right' stroke='#ffff65' reversed={false} domain={scoreDomain} />
+        <Legend verticalAlign='top' height={36} />
         <Line
           type='step'
           dot={false}
